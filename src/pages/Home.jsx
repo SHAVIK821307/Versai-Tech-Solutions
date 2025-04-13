dd092a6fa1b4aaee91ffd0bc52e16310daa5375a
SHAVIK821307 <shavikmmp@gmail.com>
2025-04-13 11:16:09 +0530

    last commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from "../components/Hero"
+import LastestCollection from '../components/LastestCollection'
+import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <LastestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
+    </div>
+  )
+}
+
+export default Home
